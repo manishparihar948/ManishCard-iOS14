4aa18cd5a0225bbce2eae10a7442bc36beae5f24
Manish Parihar <manish.parihar948@gmail.com>
2021-03-06 18:48:19 +0100

    Initial Commit

---
+++ ManishCard/ManishCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  ManishCardApp.swift
+//  ManishCard
+//
+//  Created by Manish Parihar on 06.03.21.
+//
+
+import SwiftUI
+
+@main
+struct ManishCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
